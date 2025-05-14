38f302f3f3a662618f63c6996929bf5f7e1f0c51
joemanggala <ariefmanggalaputra25@gmail.com>
2025-05-14 14:48:29 +0700

    Add next.config.ts 1747208906685

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/rezeve/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pexels.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
